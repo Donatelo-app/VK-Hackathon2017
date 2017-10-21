d3dc3f47ec9c2f3d5c5c87a977605208d1d216ce
Alexander Mamaev <alxmamaev@ya.ru>
2017-10-21 10:26:59 +0300

    Fix bitcoin

--- wallet_utils.py
+++ wallet_utils.py
@@ -7,7 +7,7 @@ def get_balance(wallets):
 def bitcoin_balance(address):
 	res = requests.get("https://blockchain.info/balance?active=%s" % address).json()
 
-	balance = res[self.address]["final_balance"]
+	balance = res[address]["final_balance"]
 	
 	res = requests.get("https://blockchain.info/ticker").json()
 
